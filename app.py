59f4a429f7ea289fa131103dd9572311c50a0349
Moscoder99 <osullivan.martina@gmail.com>
2020-07-07 09:47:10 +0000

    removed separate dbname from app config

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, requests, url_for
+from flask_pymongo inport flask_pymongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config['MONGO_URI'] = 'mongodb+srv://movieclub_movieadmin:1Evening!@cluster0-z3z20.mongodb.net/movieclubmos?retryWrites=true&w=majority'
+
+mongodb = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World....again'
+@app.route('/get_movies')
+def get_movies():
+    return 'Hello World....again...blah'
+    return render_template ("moviles.html", movies-mongo.db.movies.find)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
