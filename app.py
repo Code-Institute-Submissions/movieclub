a5cc49d699044c8fc39936803e5ebf0be17c79a8
Moscoder99 <osullivan.martina@gmail.com>
2020-07-11 23:52:14 +0000

    admin delete button and function added and tested

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-import urllib.request
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import urllib.request
 
 app = Flask(__name__)
 
@@ -11,26 +11,51 @@ app.config['MONGO_URI'] = 'mongodb+srv://movieclub_movieadmin:hneSNJhg44nImFeo@c
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
-@app.route('/get_movies')
+@app.route('/get_movies', methods=['POST', 'GET'])
 def get_movies():
     return render_template("movies.html", movies=mongo.db.movie_list.find())
+
 
 @app.route('/add_movie')
 def add_movie():
     return render_template("addmovie.html", categories=mongo.db.movie_category.find())
 
-@app.route('/insert_movie', methods=['POST'])
+
+@app.route('/insert_movie', methods=['POST', 'GET'])
 def insert_movie():
-    movies=mongo.db.movie_list
+    movies = mongo.db.movie_list
     movies.insert_one(request.form.to_dict())
     return redirect(url_for('get_movies'))
 
-@app.route('/movie_admin/<movie_id>')
-def movie_admin(movie_id):
+
+@app.route('/admin_movie_update/<movie_id>', methods=['POST', 'GET'])
+def admin_movie_update(movie_id):
     themovie = mongo.db.movie_list.find_one({"_id": ObjectId(movie_id)})
-    all_categories = mongo.db.movie_category.find()
-    return render_template("movieadmin.html", movie=themovie, category=all_categories, categories=mongo.db.movie_category.find())
+    return render_template("adminmovieupdate.html", movie=themovie, categories=mongo.db.movie_category.find())
+
+
+@app.route('/insert_admin_movie_update/<movie_id>', methods=['POST', 'GET'])
+def insert_admin_movie_update(movie_id):
+    movies = mongo.db.movie_list
+    movies.update({'_id': ObjectId(movie_id)},
+    {
+        'movie_name': request.form.get('movie_name'),
+        'movie_releasedate': request.form.get('movie_releasedate'),
+        'movie_overview': request.form.get('movie_overview'),
+        'movie_category': request.form.get('movie_category'),
+        'movie_reviewdate': request.form.get('movie_reviewdate'),
+        'movie_has_been_reviewed': request.form.get('movie_has_been_reviewed')
+    })
+    return redirect(url_for('get_movies'))
+
+
+@app.route('/admin_movie_remove/<movie_id>')
+def admin_movie_remove(movie_id):
+    mongo.db.movie_list.remove({'_id': ObjectId(movie_id)})
+    return redirect(url_for('get_movies'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
